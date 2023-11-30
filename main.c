61e2e91c3755144641755bfc837b07df72501355
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-11-30 17:16:07 +0900

    week13

--- main.c
+++ main.c
@@ -23,9 +23,26 @@ static int food_nr;
 static int festival_nr;
 
 static int player_nr;       // 플레이어 몇명
+
+
+// -----------------------player를 구조체로 선언하기-----------------------------
+typedef struct player{
+    int     energy;
+    int     position;
+    char    name[MAX_CHARNAME];
+    int     accumCredit;        // 누적학점
+    int     flag_graduate;      // player을 졸업 조건여부 따지기 위함
+} player_t;
+
+static player_t cur_player[MAX_PLAYER];
+
+
+/*
 static int player_energy[MAX_PLAYER];   // energy최대로
 static int player_position[MAX_PLAYER];
 static char player_name[MAX_PLAYER][MAX_CHARNAME];  // 2차원 배열로
+*/
+// ------------------------------------------------------------------------------
 
 //function prototypes
 #if 0
@@ -40,25 +57,49 @@ void* findGrade(int player, char *lectureName); //find the grade from the player
 void printGrades(int player); //print all the grade history of the player
 #endif
 
-void generatePlayers(int n, int initEnergy)     //플레이어 생성
+// ============================플레이어 상태 출력 함수====================================
+void printPlayerStatus(void)
+{
+    int i;
+    
+    for (i=0; i<player_nr; i++)
+    {
+        printf("%s : credit %i, energy %i, position %i\n",
+               cur_player[i].name,
+               cur_player[i].accumCredit,
+               cur_player[i].energy,
+               cur_player[i].position);
+    }
+    
+}
+// ==================================================================================
+
+
+// ===============================player 생성 함수======================================
+void generatePlayers(int n, int initEnergy)
 {
     int i;
     //n time loop
     for(i=0; i<n; i++)
     {
         //input name
-        printf();// 안내문구
-        scanf("%s", &player_name[i]);
+        printf("Input player %i's name :",i);      // player이름 입력하라고 출력
+        scanf("%s", cur_player[i].name);    // i번째 player의 이름 저장
+        fflush(stdin);
         
         //set position
-        player_position = 0;
+        cur_player[i].position = 0;
         
         // set energy
-        player_energy = initEnergy;
+        cur_player[i].energy = initEnergy;
+        cur_player[i].accumCredit = 0;
+        cur_player[i].flag_graduate = 0;
     }
 }
+// ======================================================================================
 
 
+// ================================주사위 굴리기=============================================
 int rolldie(int player)
 {
     char c;
@@ -74,20 +115,40 @@ int rolldie(int player)
     return (rand()%MAX_DIE + 1);
 }
 
-#if 0
+// =======================================================================================
+
+// =====================
 //action code when a player stays at a node
 void actionNode(int player)
 {
+    int type = smmObj_getNodeType(cur_player[player].position);
+    
     switch(type)
     {
         //case lecture:
+        case SMMNODE_TYPE_LECTURE:
+            cur_player[player].accumCredit += smmObj_getNodeCredit(cur_player[player].position);
+            cur_player[player].energy -= smmObj_getNodeEnergy(cur_player[player].position);
+            break;
+            
         default:
             break;
     }
 }
-#endif
+// ===================================================================================
 
+// ========================앞으로 가라====================
+void goForward(int player, int step)
+{
+     cur_player[player].position += step;
+     
+     printf("%s go to node %i (name: %s)\n",
+                cur_player[player].name, cur_player[player].position,
+                smmObj_getNodeName(cur_player[player].position));
+}
+// ===========================================================================
 
+// ================================================ MAIN 함수 ===================================================
 
 int main(int argc, const char * argv[]) {
     
@@ -97,6 +158,8 @@ int main(int argc, const char * argv[]) {
     int credit;
     int energy;
     int i;
+    int initEnergy;
+    int turn=0;
     
     board_nr = 0;
     food_nr = 0;
@@ -114,19 +177,23 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
-    printf("Reading board component......\n");
+    printf("Reading board component......\n");      // 파일에서 받아오기
     while ( fscanf(fp, "%s %i %i %i", name, &type, &credit, &energy) == 4 ) //read a node parameter set
     {
         //store the parameter set
         smmObj_genNode(name, type, credit, energy);
+        if (type == SMMNODE_TYPE_HOME)
+            initEnergy = energy;
         board_nr++;
     }
     fclose(fp);
     printf("Total number of board nodes : %i\n", board_nr);
     
     for (i = 0;i<board_nr;i++)
-        printf("node %i : %s, %i\n", i, smmObj_getNodeName(i), smmObj_getNodeType(i));
-    
+        printf("node %i : %s, %i(%s), credit %i, energy %i\n",
+               i, smmObj_getNodeName(i), smmObj_getNodeType(i), smmObj_getTypeName(smmObj_getNodeType(i)),
+               smmObj_getNodeCredit(i), smmObj_getNodeEnergy(i));
+    printf("(%s)", smmObj_getTypeName(SMMNODE_TYPE_LECTURE));
     #if 0
     //2. food card config
     if ((fp = fopen(FOODFILEPATH,"r")) == NULL)
@@ -159,7 +226,7 @@ int main(int argc, const char * argv[]) {
     }
     fclose(fp);
     printf("Total number of festival cards : %i\n", festival_nr);
-    
+#endif
     
     
     //2. Player configuration ---------------------------------------------------------------------------------
@@ -174,9 +241,9 @@ int main(int argc, const char * argv[]) {
     while (player_nr < 0 || player_nr > MAX_PLAYER);
     
     generatePlayers(player_nr, initEnergy);
-    generatePlayers();
+    // player 상태를 for문으로 확인해보기
     
-    
+#if 0
     //3. SM Marble game starts ---------------------------------------------------------------------------------
     while () //is anybody graduated?
     {
@@ -198,6 +265,6 @@ int main(int argc, const char * argv[]) {
         
     }
     #endif
-    system("PAUSE");
+
     return 0;
 }
--- smm_object.c
+++ smm_object.c
@@ -28,37 +28,69 @@ static char smmNodeName [MAX_NODETYPE][MAX_CHARNAME] =
 
 char* smmObj_getTypeName(int type)
 {
-    
+    return (char*)smmNodeName[type];
 }
 
-static char smmObj_name[MAX_NODE][MAX_CHARNAME];
-static int smmObj_type[MAX_NODE];
-static int smmObj_credit[MAX_NODE];
-static int smmObj_energy[MAX_NODE];
-static int smmObj_noNode=0;
+// ========================게임 보드의 구조체화====================================
 
+// 1. 구조체 형식 정의 (typedef struct문법 활용)-------------------------
+typedef struct smmObject {
+    char name[MAX_CHARNAME];
+    int type;
+    int credit;
+    int energy;
+} smmObject_t;  // type을 뜻하는 의미에서
 
+// 2. 구조체 배열 변수 정의------------------------------------------------
+smmObject_t smm_node[MAX_NODE];     // struct안해도 ㄱㅊ
+
+// 지우는 것 보다 남겨두기(컴파일은 X)
+/*
+static char     smmObj_name[MAX_NODE][MAX_CHARNAME];
+static int      smmObj_type[MAX_NODE];
+static int      smmObj_credit[MAX_NODE];
+static int      smmObj_energy[MAX_NODE];
+*/
+
+static int      smmObj_noNode=0;
+
+// 3. 관련 함수 변경-------------------------------------------------------
 //object generation
 void smmObj_genNode(char* name, int type, int credit, int energy)
 {
+    /* 구조체 이전
     strcpy(smmObj_name[smmObj_noNode], name);
     smmObj_type[smmObj_noNode] = type;
     smmObj_credit[smmObj_noNode] = credit;
     smmObj_energy[smmObj_noNode] = energy;
+    */
+    strcpy(smm_node[smmObj_noNode].name, name);
+    smm_node[smmObj_noNode].type = type;
+    smm_node[smmObj_noNode].credit = credit;
+    smm_node[smmObj_noNode].energy = energy;
     
     smmObj_noNode++;
 }
 
 char* smmObj_getNodeName(int node_nr)
 {
-    return smmObj_name[smmObj_noNode];
+    return smm_node[smmObj_noNode].name;
 }
 
 int smmObj_getNodeType(int node_nr)
 {
-    return smmObj_type[node_nr];
+    return smm_node[smmObj_noNode].type;
 }
 
+int smmObj_getNodeCredit(int node_nr)
+{
+    return smm_node[node_nr].credit;
+}
+
+int smmObj_getNodeEnergy(int node_nr)
+{
+    return smm_node[node_nr].energy;
+}
 #if 0
 
 
